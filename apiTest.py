b50030591683928013be1c4b23760ad17b750471
Dejan Ribnikar <dejanribnikar@gmail.com>
2019-04-17 18:05:46 +0300

    test commit

--- apiTest.py
+++ apiTest.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, request, jsonify, render_template
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from models import User
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +9,8 @@ api = Api(app)
 app.config.from_object(os.environ["APP_SETTINGS"])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+from models import User
 
 
 @app.route("/")
