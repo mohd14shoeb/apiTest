0809a24f3eb1b0dbf7022cbb552b68d9fd3c515a
Dejan Ribnikar <dejanribnikar@gmail.com>
2019-04-17 18:02:34 +0300

    test commit

--- apiTest.py
+++ apiTest.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify, render_template
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from models import User
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,30 +11,31 @@ app.config.from_object(os.environ["APP_SETTINGS"])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-from models import User
 
 @app.route("/")
-def usersList():
+def users_list():
     return "Hello"
 
-@app.route("/add",methods=["GET", "POST"])
+
+@app.route("/add", methods=["GET", "POST"])
 def add_user():
-    name=request.args.get("name")
-    age=request.args.get("age")
-    occupation=request.args.get("occupation")
+    name = request.args.get("name")
+    age = request.args.get("age")
+    occupation = request.args.get("occupation")
     try:
         user = User(
             name=name,
             age=age,
             occupation=occupation
         )
-        if name is None or age is None or occupation is None:
+        if name is None or age is None or occupation is (None or ""):
             return "Invalid request arguments. Use: /add?name=<name>&age=<age>&occupation=<occupation>"
         db.session.add(user)
         db.session.commit()
         return "User added. User id={}".format(user.id)
     except Exception as e:
-        return(str(e))
+        return str(e)
+
 
 @app.route("/getall")
 def get_all():
@@ -41,22 +43,24 @@ def get_all():
         users = User.query.all()
         return jsonify([e.serialize() for e in users])
     except Exception as e:
-        return(str(e))
+        return str(e)
+
 
 @app.route("/get/<id_>")
 def get_by_id(id_):
     try:
-        user=User.query.filter_by(id=id_).first()
+        user = User.query.filter_by(id=id_).first()
         return jsonify(user.serialize())
     except Exception as e:
-        return(str(e))
+        return str(e)
 
-@app.route("/add/form",methods=["GET", "POST"])
+
+@app.route("/add/form", methods=["GET", "POST"])
 def add_user_form():
     if request.method == "POST":
-        name=request.form.get("name")
-        age=request.form.get("age")
-        occupation=request.form.get("occupation")
+        name = request.form.get("name")
+        age = request.form.get("age")
+        occupation = request.form.get("occupation")
         try:
             user = User(
                 name=name,
@@ -67,8 +71,9 @@ def add_user_form():
             db.session.commit()
             return "User added. User id={}".format(user.id)
         except Exception as e:
-            return(str(e))
+            return str(e)
     return render_template("getdata.html")
+
 
 @app.route("/deleteall")
 def delete_all_users():
@@ -79,7 +84,8 @@ def delete_all_users():
         db.session.commit()
         return "Deleted all database entries."
     except Exception as e:
-        return(str(e))
+        return str(e)
+
 
 @app.route("/delete/<id_>")
 def delete_by_id(id_):
@@ -89,7 +95,7 @@ def delete_by_id(id_):
         db.session.commit()
         return "User deleted. User id={}".format(user.id)
     except Exception as e:
-        return(str(e))
+        return str(e)
 
 
 if __name__ == "__main__":
